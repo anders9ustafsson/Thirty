12628dcea7d4855104def8545493fec52074cf1a
Anders Gustafsson <anders.gustafsson@scandidos.com>
2026-08-20 07:32:07 +0200

    Remove redundant 1-die strategy

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Thirty;
 
-const int numberOfRounds = 1000000;
+const int numberOfRounds = 100000;
 const int stopAt = 30;
 const int strategyMinEyes = 4;
 
@@ -10,10 +10,9 @@ for (var d5 = d6; d5 >= strategyMinEyes; --d5)
 for (var d4 = d5; d4 >= strategyMinEyes; --d4)
 for (var d3 = d4; d3 >= strategyMinEyes; --d3)
 for (var d2 = d3; d2 >= strategyMinEyes; --d2)
-for (var d1 = d2; d1 >= strategyMinEyes; --d1)
 {
     var lockStrategy = new LockStrategy(new Dictionary<int, int>
-        { { 6, d6 }, { 5, d5 }, { 4, d4 }, { 3, d3 }, { 2, d2 }, { 1, d1 } });
+        { { 6, d6 }, { 5, d5 }, { 4, d4 }, { 3, d3 }, { 2, d2 } });
     var win = 0;
 
     // Create list of six die
@@ -22,10 +21,10 @@ for (var d1 = d2; d1 >= strategyMinEyes; --d1)
     for (var i = 0; i < numberOfRounds; i++)
     {
         var sum = Round(dice, lockStrategy, stopAt);
-        if (sum >= stopAt) win++;
+        if (sum >= 30) win++;
     }
 
-    Console.WriteLine($"Strategy {d6},{d5},{d4},{d3},{d2},{d1} wins {(double)win / numberOfRounds * 100.0:F2}%");
+    Console.WriteLine($"Strategy {d6},{d5},{d4},{d3},{d2} wins {(double)win / numberOfRounds * 100.0:F1}%");
 }
 
 return;
