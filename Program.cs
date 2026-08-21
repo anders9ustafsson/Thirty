39d5700b293ca952a4600adbb28c97d18704d809
Anders Gustafsson <anders.gustafsson@scandidos.com>
2026-08-21 10:35:35 +0200

    Compute points

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Thirty;
 
-const int numberOfRounds = 100000;
+const int numberOfRounds = 10000000;
 const int stopAt = 30;
 const int strategyMinEyes = 4;
 
@@ -14,6 +14,7 @@ for (var d2 = d3; d2 >= strategyMinEyes; --d2)
     var lockStrategy = new LockStrategy(new Dictionary<int, int>
         { { 6, d6 }, { 5, d5 }, { 4, d4 }, { 3, d3 }, { 2, d2 } });
     var win = 0;
+    var points = 0.0;
 
     // Create list of six die
     var dice = Enumerable.Range(0, 6).Select(_ => new Die()).ToList();
@@ -21,10 +22,13 @@ for (var d2 = d3; d2 >= strategyMinEyes; --d2)
     for (var i = 0; i < numberOfRounds; i++)
     {
         var sum = Round(dice, lockStrategy, stopAt);
-        if (sum >= 30) win++;
+        if (sum >= stopAt) win++;
+        
+        // Minus points if below 30, 10 bonus if all dice have the same eyes
+        points += Math.Min(0, sum - 30) + (dice.All(die => die == dice[0]) ? 10 : 0);
     }
 
-    Console.WriteLine($"Strategy {d6},{d5},{d4},{d3},{d2} wins {(double)win / numberOfRounds * 100.0:F1}%");
+    Console.WriteLine($"Strategy {d6},{d5},{d4},{d3},{d2} wins {(double)win / numberOfRounds * 100.0:F1}%, average points {points / numberOfRounds:F3}");
 }
 
 return;
