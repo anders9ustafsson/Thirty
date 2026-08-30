973469600cf74eb6597ebfb1a15ac8f7ea15f352
Anders Gustafsson <anders.gustafsson@scandidos.com>
2026-08-30 11:37:20 +0200

    Allow re-evaluation

--- LockStrategy.cs
+++ LockStrategy.cs
@@ -1,8 +1,8 @@
 namespace Thirty;
 
-public class LockStrategy(Dictionary<int, int> minEyesForNumberUnlocked, int stopAt)
+public class LockStrategy(Dictionary<int, int> minEyesForNumberUnlocked, int stopAt, bool reEvaluate)
 {
-    public LockStrategy(int minEyes) : this(Enumerable.Range(1, 6).ToDictionary(i => i, _ => minEyes), 30)
+    public LockStrategy(int minEyes) : this(Enumerable.Range(1, 6).ToDictionary(i => i, _ => minEyes), 30, false)
     {
     }
 
@@ -10,18 +10,23 @@ public class LockStrategy(Dictionary<int, int> minEyesForNumberUnlocked, int sto
 
     public bool Apply(List<Die> dice)
     {
-        // Get min eyes for the number of unlocked dice
-        if (!minEyesForNumberUnlocked.TryGetValue(dice.Count(die => !die.Locked), out var minEyes)) return false;
-
         var anyLocked = false;
-        foreach (var die in dice)
-            if (die.LockIfAtLeast(minEyes))
-                anyLocked = true;
+
+        int count;
+        do
+        {
+            // Get min eyes for the number of unlocked dice
+            if (!minEyesForNumberUnlocked.TryGetValue(dice.Count(die => !die.Locked), out var minEyes))
+                return anyLocked;
+
+            if ((count = dice.Count(die => die.LockIfAtLeast(minEyes))) > 0) anyLocked = true;
+        } while (reEvaluate && count > 0);
+
         return anyLocked;
     }
 
     public override string ToString()
     {
-        return $"{string.Join(",", minEyesForNumberUnlocked.Values)},{StopAt}";
+        return $"{string.Join(",", minEyesForNumberUnlocked.Values)},{StopAt},{reEvaluate}";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 ﻿using Thirty;
 
-PlayStatistics();
+SinglePlay();
 return;
 
-static IEnumerable<LockStrategy> EnumerateLockStrategies(int minEyes, int stopAt)
+static IEnumerable<LockStrategy> EnumerateLockStrategies(int minEyes, int stopAt, bool reEvaluate)
 {
     for (var d6 = 6; d6 >= minEyes; --d6)
     for (var d5 = d6; d5 >= minEyes; --d5)
     for (var d4 = d5; d4 >= minEyes; --d4)
     for (var d3 = d4; d3 >= minEyes; --d3)
     for (var d2 = d3; d2 >= minEyes; --d2)
+    for (var d1 = d2;
+         reEvaluate ? d1 >= minEyes : d1 == d2;
+         --d1) // Only meaningful to decrement d1 if reEvaluate is true
     {
         yield return new LockStrategy(new Dictionary<int, int>
-            { { 6, d6 }, { 5, d5 }, { 4, d4 }, { 3, d3 }, { 2, d2 } }, stopAt);
+            { { 6, d6 }, { 5, d5 }, { 4, d4 }, { 3, d3 }, { 2, d2 }, { 1, d1 } }, stopAt, reEvaluate);
     }
 }
 
@@ -23,7 +26,7 @@ static void CreateStatistics()
     const int numberOfRounds = 100000;
 
     // Loop over relevant lock strategies
-    foreach (var lockStrategy in EnumerateLockStrategies(strategyMinEyes, stopAt))
+    foreach (var lockStrategy in EnumerateLockStrategies(strategyMinEyes, stopAt, true))
     {
         var player = new Player("Player", lockStrategy);
 
@@ -52,7 +55,7 @@ static void PlayStatistics()
     const int numberOfPlays = 1000;
     const int stopAt = 30;
     const int strategyMinEyes = 4;
-    var lockStrategiesWins = EnumerateLockStrategies(strategyMinEyes, stopAt).ToDictionary(ls => ls, _ => 0);
+    var lockStrategiesWins = EnumerateLockStrategies(strategyMinEyes, stopAt, false).ToDictionary(ls => ls, _ => 0);
 
     for (var i = 0; i < lockStrategiesWins.Count - 1; i++)
     for (var j = i + 1; j < lockStrategiesWins.Count; j++)
@@ -82,4 +85,31 @@ static void PlayStatistics()
     {
         Console.WriteLine($"{lockStrategyWins.Key} total wins: {lockStrategyWins.Value}");
     }
+}
+
+static void SinglePlay()
+{
+    const int numberOfPlays = 100000;
+
+    var lockStrategy1 = new LockStrategy(new Dictionary<int, int>
+        { { 6, 6 }, { 5, 6 }, { 4, 6 }, { 3, 5 }, { 2, 5 }, { 1, 5 } }, 34, false);
+    var lockStrategy2 = new LockStrategy(new Dictionary<int, int>
+        { { 6, 6 }, { 5, 6 }, { 4, 6 }, { 3, 5 }, { 2, 5 }, { 1, 5 } }, 34, true);
+    
+    var player1 = new Player("Player 1", lockStrategy1);
+    var player2 = new Player("Player 2", lockStrategy2);
+
+    var wins1 = 0;
+    var wins2 = 0;
+
+    var game = new Game([player1, player2]);
+
+    for (var i = 0; i < numberOfPlays; i++)
+    {
+        var winner = game.Play();
+        if (winner == player1) wins1++;
+        else if (winner == player2) wins2++;
+    }
+
+    Console.WriteLine($"{player1.LockStrategy} wins: {wins1}, {player2.LockStrategy} wins: {wins2}");
 }
